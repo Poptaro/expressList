f502ddfa627edf7be61efe938d337bfb374f8a9f
root <root@Potato>
2024-02-11 16:48:13 -0600

    Functional api based todolist

--- express.js
+++ express.js
@@ -1,60 +1,64 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const app = express();
+const { createId } = require("@paralleldrive/cuid2");
 
 //allows the use of req.body to parse it to JSON apparently
-app.use(express.json())
+app.use(express.json());
 
 // homepage
-app.get('/', function (req, res) {
-    app.use(express.static(path.join(__dirname)));
-    res.sendFile(path.join(__dirname, 'todo.html'));
+app.get("/", function (req, res) {
+  app.use(express.static(path.join(__dirname)));
+  res.sendFile(path.join(__dirname, "todo.html"));
 });
 
-
-let todolist = [
-    {
-        "id": 1,
-        "item": 'buy bread',
-        "password": 'peanutbuttuh'
-    },
-    {
-        "id": 2,
-        "item": 'watch 90 movies',
-        "password": 'chunkymonkey'
-    }
-];
+let todolist = [];
 
 //acquire whats on the list AT THE MOMENT
-app.get('/list', (req, res) => {
-    return res.json(todolist.map(list => ({id: list.id, item: list.item})))
-})
+app.get("/list", (req, res) => {
+  return res.json(todolist);
+});
 
 //acquire certain todo list item
-app.get('/list/:id', (req, res) => {
-    const { id } = req.params
-    return res.json(
-        todolist
-        .filter(list => list.id == id)
-        .map(list => ({id: list.id, item: list.item})))
-          
-})
+app.get("/list/:id", (req, res) => {
+  const { id } = req.params;
+  return res.json(
+    todolist
+      .filter(list => list.id == id)
+      .map(list => ({ id: list.id, item: list.item }))
+  );
+});
 
 //utilizing httPie, you can add objects via the body system
-app.post('/list', (req, res) => {
-    const newListItem = req.body;
-    todolist.push(newListItem)
-    
-    return res.json(todolist)
-})
+app.post("/list", (req, res) => {
+  const todo = req.body;
+  todo.id = createId();
+  todolist.push(todo);
+
+  return res.json(todolist);
+});
 
 //grab every list item besides the id'd one and make the old todolist the list you just grabbed. return todolist for visual sake
-app.delete('/list/:id', (req, res) => {
-    const { id } = req.params
-    list = todolist.filter(item => item.id != id)
-    todolist = list
-    return res.json(todolist)
-})
+app.delete("/list/:id", (req, res) => {
+  const { id } = req.params;
+  list = todolist.filter(item => item.id != id);
+  todolist = list;
+  return res.status(201).json(null);
+});
+
+app.patch("/list/:id", (req, res) => {
+  // get the id from the request
+  const id = req.params.id;
+
+  // find the list item with the id
+  const itemIndex = todolist.findIndex(item => item.id == id);
+
+  // modify the list item
+  todolist[itemIndex].item = req.body.item;
+
+  // return the list item
+  return res.json(todolist[itemIndex]);
+});
 
 const port = 3000;
 app.listen(port);
--- todo.js
+++ todo.js
@@ -1,5 +1,8 @@
-const unorderedList = document.querySelector('.ulList')
-const startButton = document.querySelector('.startButton')
+const unorderedList = document.querySelector('#ulList')
+const startButton = document.querySelector('#startButton')
+const listInput = document.querySelector('#listInput')
+const formInput = document.querySelector('#formInput')
+
 
 
 async function acquireList() {
@@ -8,12 +11,93 @@ async function acquireList() {
     return listResults
 }
 
-startButton.addEventListener('click', async () => {
+async function postItem() {
+    let todo = { "item": `${listInput.value}`};
+    await fetch ("http://localhost:3000/list", {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(todo)
+    })
+    listInput.value = ''
+}
+
+async function removeListItem(id) {
+    await fetch(`http://localhost:3000/list/${id}`, {
+        method: "DELETE",
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    getTodo()
+}
+
+async function editListItem(id) {
+    console.log(id)
+    let userInput = prompt("What do you want to change this to?")
+    let todo = { "item": `${userInput}`}
+    console.log(todo)
+    await fetch(`http://localhost:3000/list/${id}`, {
+        method: "PATCH",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(todo)
+    })
+    getTodo()
+}
+
+async function getTodo() {
     const listData = await acquireList()
+    clearTodo()
     listData.forEach((obj) => {
+        //create the list item
         const listItem = document.createElement('li')
+        listItem.className = "listItem"
         listItem.textContent = obj.item
         unorderedList.appendChild(listItem)
+        
+        //create buttons to remove the list item
+        const removeButton = document.createElement('button')
+        removeButton.className = "btn btn-danger btn-sm"
+        removeButton.innerHTML = 'REMOVE'
+        removeButton.onclick = () => removeListItem(obj.id)
+        listItem.appendChild(removeButton)
+
+        //edit list item
+        const editButton = document.createElement('button')
+        editButton.className = "btn btn-info btn-sm"
+        editButton.innerHTML = 'EDIT'
+        editButton.onclick = () => editListItem(obj.id)
+        listItem.appendChild(editButton)
+
+
     })
+}
+
+function clearTodo() {
+ unorderedList.innerHTML = '';
+}
+
+
+
+document.addEventListener('DOMContentLoaded', async () => {
+    getTodo()
 })
 
+startButton.addEventListener('click', () => {
+    if(listInput.value.length > 1){
+        postItem()
+        getTodo()
+    }
+    
+})
+
+formInput.addEventListener('submit', (event) => {
+    event.preventDefault()
+    if(listInput.value.length > 1){
+        postItem()
+        getTodo()
+    }
+})
